06754b0fd82a3f56643d21791f80af1762d53cb7
Alex-bensimon <64471842+Alex-bensimon@users.noreply.github.com>
2020-11-23 12:08:41 +0100

    new test workflow

--- src/main/scala/com/fakir/samples/SampleProgram.scala
+++ src/main/scala/com/fakir/samples/SampleProgram.scala
@@ -20,6 +20,7 @@ object SampleProgram {
     Logger.getLogger("org").setLevel(Level.OFF)
     val sparkSession = SparkSession.builder().master("local").getOrCreate()
 
+    /*
   // EXERCICE 1
 
     // Q1 :
@@ -45,10 +46,9 @@ object SampleProgram {
 
     // Q5 :
 
+  */
 
 
-
-  /*
     // EXERCICE 2 :
 
     // Q1 :
@@ -61,13 +61,16 @@ object SampleProgram {
     df_renamed.printSchema()
 
     // Q3 :
+    // Q3.2
     val new_df = df_renamed.filter(col(colName = "acteur_principal")==="Di Caprio")
     val nb = new_df.count()
     println(nb)
 
+    // Q3.3
     val mean = new_df.groupBy("acteur_principal").mean("note_film")
     mean.show
 
+    // Q3.4
     val sum = new_df.groupBy("acteur_principal").sum("nombre_vues")
     sum.show
 
@@ -80,79 +83,16 @@ object SampleProgram {
     val rate_mean = df_renamed.groupBy("acteur_principal").mean("note_film")
     rate_mean.show
 
+    // Q3.5
     val view_mean = df_renamed.groupBy("acteur_principal").mean("nombre_vues")
     view_mean.show
 
+    // Q4 :
     val new_column = df_renamed.withColumn("pourcentage_de_vues", (col("nombre_vues") / sum_tot)*100)
     new_column.show()
-  */
 
-    // Q1 :
-    //val df: DataFrame = sparkSession.read.option("delimiter", ",").option("inferSchema", true).option("header", true).csv("films.csv")
-    //df.show()
-      /*
-    // Utilisation de l'UDF
-    val transformDateDf = df.withColumn(colName="Order Date", transformField(col(colName="Order Date")))
-    transformDateDf.printSchema()
-    transformDateDf.show()
 
-    // Q2 : produits avec un prix unitaire > 500€ et plus de 3000 unités vendues
-    val new_df = df.filter(col(colName = "Unit Price") > 500  && col(colName = "Units Sold") > 3000)
-    val nb = new_df.count()
-    println(nb)
 
-    //Question 3 : Faites la somme de tous les produits vendus valant plus de $500 en prix unitaire
-    val sum = df.filter(col("Unit Price") >= 100).groupBy("Unit Price").sum("Units Sold")
-    sum.show
 
-    //Question 4 : Quel est le prix moyen de tous les produits vendus ? (En groupant par item_type)
-    val mean = df.groupBy("Item Type").mean("Unit Price")
-    mean.show
-
-    //Question 5 : Créer une nouvelle colonne, "total_revenue" contenant le revenu total des ventes par produit vendu.
-    val revenue = df.withColumn("total_revenue", col("Units Sold") * col("Unit Price"))
-    //revenue.show
-    //Question 6 : Créer une nouvelle colonne, "total_cost", contenant le coût total des ventes par produit vendu.
-    val cost = revenue.withColumn("total_cost", col("Units Sold") * col("Unit Cost"))
-    //cost.show
-    //Question 7 : Créer une nouvelle colonne, "total_profit", contenant le bénéfice réalisé par produit.
-    val profit = cost.withColumn("Total Profit", col("total_revenue") - col("total_cost"))
-
-    val discount = profit.withColumn("unit_price_discount", when(col("Units Sold") > 3000, col("Unit Price") * 0.7).otherwise(col("Unit Price") * 0.9))
-    //Question8
-    val q8 = discount.withColumn("total_revenue", col("Units Sold") * col("unit_price_discount"))
-    //Question 9
-    val q9 = q8.withColumn("total_profit", when(col("Units Sold") > 3000, col("Units Sold") * (col("Unit Price") - col("Unit Cost"))))
-
-    // Question 10
-    val columnsList = df.columns
-    val columnsWithoutSpaces : Array[String] = columnsList.map(elem => elem.replaceAll(" ","_"))
-
-    val dfWithRightColumnsNames = df.toDF(columnsWithoutSpaces:_*)
-    //dfWithRightColumnsNames.show()
-    dfWithRightColumnsNames.write.partitionBy("Sales_Channel").mode(SaveMode.Overwrite).parquet("result")
-    val parquetDF = sparkSession.read.parquet("result")
-    parquetDF.printSchema()
-    parquetDF.show
-
-       */
   }
-  /*
-  def main(args: Array[String]): Unit = {
-    Logger.getLogger("org").setLevel(Level.OFF)
-
-    val sparkSession = SparkSession.builder().master("local").getOrCreate()
-
-
-    val rdd = sparkSession.sparkContext.textFile("{data/dataPop.csv}")
-    val filter_rdd = rdd.filter(elem => !elem.startsWith("H") || elem.split(";")(1).toDouble > 2)
-    val majRDD = filter_rdd.map((elem: String) => elem.split(";")(2))
-
-
-    val counts = rdd.map(item => (item.split(";")(2).toDouble, (1.0, item.split(";")(1).toDouble)))
-    val countSums = counts.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
-    val keyMeans = countSums.mapValues(avgCount => avgCount._2 / avgCount._1)
-    keyMeans.foreach(println)
-  }
-  */
 }
